84fae72686d36005925a191a7cf183050789ef06
Limazxz <goubulsegue@gmail.com>
2024-08-21 15:22:23 -0300

    finalizado o docionário

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -12,58 +12,70 @@
         frutas.Add("Goiaba");
         frutas.Add("Uva");
 
-        frutas.ForEach(Console.WriteLine);
         /* imprmir os itens da lista */
-        //Console.WriteLine("Minha Lista de Frutas");
-        //foreach (string fruta in frutas)
-        //{
-        //    Console.WriteLine(fruta);
-        //}
-        //Console.WriteLine(); //pula linha em branco
+        frutas.ForEach(Console.WriteLine);
 
-        ///* imprimir elemento da indice específica */
-        //Console.WriteLine("Fruta no indice 2: " + frutas[2]);
+        Console.WriteLine(); //pula linha em branco
 
-        ///* iserir um elemento no indice específico */
-        //frutas.Insert(1, "Maracujá");
+        /* imprimir elemento da indice específica */
+        Console.WriteLine("Fruta no indice 2: " + frutas[2]);
 
-        ///* imprimindo a lista novamente */
-        //Console.WriteLine();
+        /* iserir um elemento no indice específico */
+        frutas.Insert(1, "Maracujá");
 
-        //Console.WriteLine("Minha Lista de Frutas");
-        //foreach (string fruta in frutas)
-        //{
-        //    Console.WriteLine(fruta);
-        //}
+        /* imprimindo a lista novamente */
+        Console.WriteLine();
 
-        ///* alterar um elemento no indice específico */
+        Console.WriteLine("Minha Lista de Frutas");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
 
-        //frutas[4] = "Pêra";
-        ///* imprimindo a lista novamente */
-        //Console.WriteLine();
+        /* alterar um elemento no indice específico */
 
-        //Console.WriteLine("Minha Lista de Frutas");
-        //foreach (string fruta in frutas)
-        //{
-        //    Console.WriteLine(fruta);
-        //}
+        frutas[4] = "Pêra";
+        /* imprimindo a lista novamente */
+        Console.WriteLine();
 
-        ///* remover elementos da lista */
+        Console.WriteLine("Minha Lista de Frutas");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
 
-        //frutas.RemoveAt(3);
-        ///* remover elementos pelo valor do contúudo */
-        //frutas.Remove("Morango");
-        ///* imprimindo a lista novamente */
-        //Console.WriteLine();
+        /* remover elementos da lista */
 
-        //Console.WriteLine("Minha Lista de Frutas");
-        //foreach (string fruta in frutas)
-        //{
-        //    Console.WriteLine(fruta);
-        //}
+        frutas.RemoveAt(3);
+        /* remover elementos pelo valor do contúudo */
+        frutas.Remove("Morango");
+        /* imprimindo a lista novamente */
+        Console.WriteLine();
 
-        ///* apagar todos os elementos da lista */
-        //frutas.Clear();
+        Console.WriteLine("Minha Lista de Frutas");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
 
+        /* apagar todos os elementos da lista */
+        frutas.Clear();
+
+        Dictionary<int, string> carros = new Dictionary<int, string>();
+        /* adicionar dados a um dicionário */
+        carros.Add(5, "Corsa");
+        carros.Add(10, "Fusca");
+        carros.Add(2, "Ford Ka");
+
+        Console.WriteLine();
+        Console.WriteLine("===========================");
+        Console.WriteLine();
+
+        Console.WriteLine("Meu Dicionário de Carros");
+        /* imprimir um sicionário de dados */
+        foreach (var carro in carros)
+        {
+            Console.WriteLine($"{carro.Key} - {carro.Value}");
+        }
     }
 }
